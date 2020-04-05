17766a2966149f77eefabb9fa3379f921546eec7
RodFox <rodfox.developers@gmail.com>
2020-04-05 10:46:13 +0200

    final commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,17 @@
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DefaultComponent } from './layouts/default/default.component';
 import { DashboardComponent } from './modules/dashboard/dashboard.component';
 import { PostsComponent } from './modules/posts/posts.component';
 import { ContactsComponent } from './modules/contacts/contacts.component';
 import { FaqComponent } from './modules/faq/faq.component';
 import { SettingsComponent } from './modules/settings/settings.component';
+import { AppComponent } from './app.component';
 
 
 
 const routes: Routes = [{
   path: '',
-  component: DefaultComponent,
-  children: [{
-    path: '',
-    component: DashboardComponent
+  component: DashboardComponent,
   }, {
     path: 'posts',
     component: PostsComponent
@@ -27,8 +24,8 @@ const routes: Routes = [{
   }, {
     path: 'settings',
     component: SettingsComponent
-  }]
-}];
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,51 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DefaultModule } from './layouts/default/default.module';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { ContactsComponent } from './modules/contacts/contacts.component';
+import { SettingsComponent } from './modules/settings/settings.component';
+import { FaqComponent } from './modules/faq/faq.component';
+import { PostCreateComponent } from './modules/posts/post-create/post-create.component';
+import { PostListComponent } from './modules/posts/post-list/post-list.component';
+import { PostsComponent } from './modules/posts/posts.component';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from './shared/shared.module';
+import { HighchartsChartModule } from 'highcharts-angular';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from './material/material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DashboardService } from './modules/dashboard.service';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ContactsComponent,
+    SettingsComponent,
+    FaqComponent,
+    PostCreateComponent,
+    PostListComponent,
+    PostsComponent
     ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    DefaultModule
+    CommonModule,
+    RouterModule,
+    SharedModule,
+    HighchartsChartModule,
+    FlexLayoutModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    DashboardService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
